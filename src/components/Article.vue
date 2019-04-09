38a630af7c108cdfc2ac3e373df5285325140a84
Bulat Nasibullin <nasibullinbulat95@gmail.com>
2019-04-09 12:38:33 +0400

    Added axios

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,20 +1,38 @@
 <template>
   <div>
-    {{msg}}
-    {{id}}
+    <router-link to="/articles">Назад</router-link>
+    </br>
+    {{article.title.rendered}}
+    </br>
+    <div v-html="`${article.content.rendered}`"></div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'Article',
     data () {
       return {
-        msg: 'Article'
+        article: {}
       }
     },
     props: {
       id: String
+    },
+    methods: {
+      loadArticle() {
+        const url = `https://lifehacker.ru/api/wp/v2/posts/${this.id}`;
+        axios.get(url)
+          .then((response) => {
+            this.article = response.data;
+          })
+          .catch((err) => global.console.log(err))
+      }
+    },
+    mounted() {
+      this.loadArticle();
     }
   }
 </script>
--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,20 +1,46 @@
 <template>
   <div>
-    {{msg}}
+    <ul class="article-list">
+      <li v-for="article in articles">
+        <img :src="`${article.cat_cover.sizes.mobile}`"/>
+        <router-link :to="`articles/${article.id}`"
+                     :key="`${article.id}`">
+          {{article.title.rendered}}
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
+
   export default {
     name: 'ArticleList',
     data () {
       return {
-        msg: 'ArticleList'
+        msg: 'ArticleList',
+        articles: []
       }
+    },
+    methods: {
+      loadData() {
+        const url = 'https://lifehacker.ru/api/wp/v2/posts';
+        axios.get(url)
+          .then( (response) => {
+            this.articles = response.data;
+          })
+          .catch( (err) => global.console.error(err))
+      }
+    },
+    mounted() {
+      this.loadData();
     }
   }
 </script>
 
 <style>
-
+  .article-list {
+    list-style: none;
+  }
 </style>
